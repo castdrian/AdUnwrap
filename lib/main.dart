a617e09fafdf51c9b9d3fa768690bbaee7cf2ff2
adrifcastr <adrifcastr@gmail.com>
2022-10-23 11:59:19 +0200

    add bg service

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:optimize_battery/optimize_battery.dart';
+import 'package:move_to_background/move_to_background.dart';
+import 'package:flutter_background/flutter_background.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,27 +22,53 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// ignore: must_be_immutable
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
+  String serviceStatus = "Press the button to launch the AdUnwrap service";
+  Icon buttonIcon = Icon(Icons.launch_outlined);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  void _startService() {
+  Future<void> _startService() async {
+    final androidConfig = FlutterBackgroundAndroidConfig(
+        notificationTitle: "AdUnwrap Service",
+        notificationText: "Monitoring clipboard for ad links",
+        notificationImportance: AndroidNotificationImportance.Default,
+        notificationIcon: AndroidResource(name: 'launcher_icon', defType: 'drawable'),
+    );
+    bool success = await FlutterBackground.initialize(androidConfig: androidConfig);
+
     OptimizeBattery.isIgnoringBatteryOptimizations().then((onValue) {
-        setState(() {
-            if (onValue) {
-                Fluttertoast.showToast(
-                    msg: "AdUnwrap service running",
-                );
-            } else {
-                OptimizeBattery.stopOptimizingBatteryUsage();
-            }
-        });
+      setState(() {
+        if (onValue) {
+          if (FlutterBackground.isBackgroundExecutionEnabled) {
+            FlutterBackground.disableBackgroundExecution();
+            setState(() {
+              widget.serviceStatus = "Press the button to launch the AdUnwrap service";
+              widget.buttonIcon = Icon(Icons.launch_outlined);
+            });
+            Fluttertoast.showToast(msg: "AdUnwrap service stopped");
+          } else {
+              if (success) {
+                setState(() {
+                  widget.serviceStatus = "Press the button to stop the AdUnwrap service";
+                  widget.buttonIcon = Icon(Icons.cancel_outlined);
+                });
+                FlutterBackground.enableBackgroundExecution();
+                Fluttertoast.showToast(msg: "AdUnwrap service launched");
+                MoveToBackground.moveTaskToBack();
+              }
+          }
+        } else {
+          OptimizeBattery.stopOptimizingBatteryUsage();
+        }
+      });
     });
   }
 
@@ -55,15 +83,15 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Press the button to launch the AdUnwrap service',
+              widget.serviceStatus
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _startService,
-        tooltip: 'Launch',
-        child: Icon(Icons.launch_outlined),
+        tooltip: 'AdUnwrap Service',
+        child: widget.buttonIcon,
       ),
     );
   }
